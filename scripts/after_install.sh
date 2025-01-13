d5d6946823db367bdedc7163d39d10781b6a29cf
Gauthamdevops <gauthamgowda550@gmail.com>
2025-01-13 05:49:58 +0000

    pushing updated scripts folder

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-# after_install.sh
-
-# Start Nginx if it's not running
-if ! systemctl is-active --quiet nginx; then
-    echo "Nginx is not running. Starting Nginx..."
-    sudo systemctl start nginx
-else
-    echo "Nginx is already running."
+# Example: Check if nginx is installed or configure settings
+if ! command -v nginx &> /dev/null
+then
+    yum install -y nginx
 fi
 
-# Enable Nginx to start on boot
-sudo systemctl enable nginx
